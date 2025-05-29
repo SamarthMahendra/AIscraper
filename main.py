5886f3dc1f3a0abef9e8458ed22aa85e47421396
Samarth <samarth.mahendragowda@gmail.com>
2025-05-29 11:57:05 +0530

    modified code to check status of NOC from RTO india

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import asyncio
 
 llm = ChatOpenAI(model="gpt-4o")
 
+
 async def main():
     agent = Agent(
-        task="Compare the price of gpt-4o and DeepSeek-V3",
+        task="visit 'https://vahan.parivahan.gov.in/vahanservice/vahan/ui/appl_status/form_Know_Appl_Status.xhtml' and toggle to vehicle no and then Check status of application KAXXXXXX, Issue of NOC, click on 'Click here for details' and extract status",
+        use_vision=True,
         llm=llm,
     )
     result = await agent.run()
